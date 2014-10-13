9790eb8de0a092f982fbb90144e18d0ee165ac36
Stephen Price <sprice@monsooncommerce.com>
2014-10-12 23:07:12 -0700

    fix mysql setup

--- scripts/start.sh
+++ scripts/start.sh
@@ -23,7 +23,7 @@ if [ ! -f /mysql-configured ]; then
 
 	 mysql -uroot -p"$MYSQL_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS zabbix CHARACTER SET utf8;"
 
-	 zabbix_mysql_v="/usr/share/zabbix-mysql"
+	 zabbix_mysql_v="/usr/share/doc/zabbix-server-mysql-2.4.1/create"
 
 	 mysql -uroot -D zabbix -p"$MYSQL_PASSWORD" < "${zabbix_mysql_v}/schema.sql"
 	 mysql -uroot -D zabbix -p"$MYSQL_PASSWORD" < "${zabbix_mysql_v}/images.sql"
